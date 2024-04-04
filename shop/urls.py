7b90442cd0b68ba4a90021a6d80d1684158cc0be
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2024-04-04 16:53:16 +0330

    login user
    this part we are create a login and log out user in site
    
    to split templates such as about.html

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,7 @@ app_name = 'shop'
 
 urlpatterns = [
     path('',views.hello_world, name='shop'),
+    path('about/',views.about,name='about'),
+    path('logout/',views.logout_user,name='logout'),
+    path('login/',views.login_user,name='login'),
 ]
